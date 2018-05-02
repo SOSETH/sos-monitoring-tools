cca07869756dc9c5f7b4c434b8f14b616b631774
Maximilian Falkenstein <mfalkenstein@sos.ethz.ch>
2018-05-02 21:40:35 +0200

    Initial import. Lot's of things missing right now...

---
+++ README.md
@@ -0,0 +1,11 @@
+# sos-monitoring-tools
+Tools and scripts required by SOSETH monitoring roles. This is a work in progress,
+not all tools have been packaged yet.
+
+## Usage
+Install dependencies (TODO: List deps) and do
+```
+./build.sh
+```
+After a while, you should end up with a bunch of debian packages in out, which
+you can then publish in some repository (or use ours).
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ ! -d out ] ; then
+    mkdir out
+fi
+
+set -e
+
+function buildpkg {
+    if [ ! -f "out/${1}" ] ; then
+        cd "${2}"
+        dpkg-buildpackage -b -us -uc
+        cd ..
+        mv *.deb out/
+    fi
+}
+
+echo "Building SOS monitoring tools on $(uname -a)"
+echo "Target: $(cat /etc/os-release)"
+
+# Shell
+buildpkg check-apcupsd_2.6-1_all.deb check_apcupsd
+buildpkg check-iostat_0.0.5_all.deb check_iostat
+
+# Python
+buildpkg check-sas-smart_0.1_amd64.deb check_sas_smart
+buildpkg check-power_0.1_amd64.deb check_power
+buildpkg check-mem_0.1-1_all.deb check_mem
+
+# C++
+buildpkg check-ib_0.1_amd64.deb check_ib
+
+# Perl
+buildpkg check-ipmi-sensor_3.12-1_all.deb check_ipmi_sensor_v3
+buildpkg check-lm-sensors_4.4.1-36e453f-1_all.deb check_lm_sensors
+
+# Golang
+# To prevent issues with user-supplied go environments (aka go version != 1.7
+# on strech), we reset the environment to a known, clean state
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+export GOPATH="$(pwd)/gosrc"
+if [ ! -d gosrc ] ; then
+    mkdir gosrc
+    # We also need dep since we don't include the vendor dir in GIT
+    go get -u github.com/golang/dep/cmd/dep
+fi
+
+export GOPATH=""
+export GOROOT=""
+go version
+
+buildpkg check-ceph_0.1_amd64.deb check-ceph
+
+# vim: set ts=4 sw=4 tw=0 et :
